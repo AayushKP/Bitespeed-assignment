d066e16c76adf64cd7aee9c07eb7c0ffec63c231
Ayush Kashyap <ayushprokashyap@gmail.com>
2025-02-09 14:58:18 +0530

    route defined

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,37 @@ app.use(
 );
 const port = process.env.PORT || 3000;
 
+app.post("/identify", async (req: Request, res: Response) => {
+  try {
+    const { email } = req.body;
+    let { phoneNumber } = req.body;
+    phoneNumber = phoneNumber ? phoneNumber.toString() : null;
+
+    if (!email && !phoneNumber) {
+      res
+        .status(400)
+        .json({ Error: "either email or phone number is required" });
+      return;
+    }
+
+    const contacts: Contact[] = await prisma.contact.findMany({
+      where: {
+        OR: [
+          { email: email || undefined },
+          { phoneNumber: phoneNumber || undefined },
+        ],
+      },
+      orderBy: { createdAt: "asc" },
+    });
+
+    res.status(200).json({ contacts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+    return;
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
